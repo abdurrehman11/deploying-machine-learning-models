e0c8070868ed73e379278129ab5f604ddbd5bd86
abdurrehman <abdurrehman245@gmail.com>
2020-06-17 17:51:11 +0500

    Setup Differential Tests

--- packages/regression_model/regression_model/config/config.py
+++ packages/regression_model/regression_model/config/config.py
@@ -86,3 +86,6 @@ CATEGORICAL_NA_NOT_ALLOWED = [
 
 PIPELINE_NAME = "lasso_regression"
 PIPELINE_SAVE_FILE = f"{PIPELINE_NAME}_output_v"
+
+# used for differential testing
+ACCEPTABLE_MODEL_DIFFERENCE = 0.05
